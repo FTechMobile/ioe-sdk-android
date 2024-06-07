f257c92d7f20eb8e9cd1405153e3c509b87b8880
HiepUH <hiepuh@ftech.ai>
2024-06-07 13:58:17 +0700

    1.0.5

--- app/src/main/java/com/mobile/demoioesdk/domain/model/Alphabet.kt
+++
@@ -1,8 +0,0 @@
-package com.mobile.demoioesdk.domain.model
-
-data class Alphabet(
-    var text: String = "",
-    var score: Int = 0,
-    var audioAlphabet: String? = "",
-    var description: String? = ""
-)
--- app/src/main/java/com/mobile/demoioesdk/domain/model/Word.kt
+++
@@ -1,7 +0,0 @@
-package com.mobile.demoioesdk.domain.model
-
-data class Word(
-    var id: Int = 0,
-    var text: String = "",
-    var score: Int = 0
-)
