9ebe431b899ec0537450a4a108e26d12cdfce929
HiepUH <hiepuh@ftech.ai>
2024-07-23 11:30:30 +0700

    1.1.0

--- ioesdk/src/main/java/ai/ftech/ioesdk/data/source/remote/network/ApiConfig.kt
+++ ioesdk/src/main/java/ai/ftech/ioesdk/data/source/remote/network/ApiConfig.kt
@@ -1,8 +1,8 @@
 package ai.ftech.ioesdk.data.source.remote.network
 
 object ApiConfig {
-    const val BASE_URL_IOE = "https://fcloud-streaming-socket-api.dev.ftech.ai"
-    const val BASE_URL_INIT_IOE = "https://fcloud-api-gateway.dev.ftech.ai"
+    const val BASE_URL_IOE = "https://fcloud-streaming-socket-api.ftech.ai"
+    const val BASE_URL_INIT_IOE = "https://fcloud-api-gateway.ftech.ai"
 
     object HeaderName {
         const val CONTENT_TYPE = "Content-Type"
